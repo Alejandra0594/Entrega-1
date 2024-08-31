4de486599753244f11428dc219214f287953ea4e
Alejandra <alegn.0594@gmail.com>
2024-08-31 15:23:22 -0300

    Desafiate

--- js/login.js
+++ js/login.js
@@ -1,30 +1,21 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-    
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-
-}
-
-function iniciar(){
+function iniciar() {
+    // Captura los valores de los campos de nombre de usuario y contraseña
     const username = document.getElementById('nombre_usuario').value;
     const password = document.getElementById('contraseña_usuario').value;
 
+    // Verifica si ambos campos están llenos
     if (username && password) {
-        // Si inició sesión
-        sessionStorage.setItem('sesion', 'true');
-        // lo mando al index.html
-        window.location.href = 'index.html';
-    } else  {
-        showAlertError();
-        return;
-    }
-        
-    
+        // Guarda el estado de sesión y el nombre de usuario en sessionStorage
+        sessionStorage.setItem('sesion', 'true');  // Guarda que el usuario inició sesión
+        sessionStorage.setItem('nombreUsuario', username);  // Guarda el nombre de usuario
 
+        // Redirige al usuario a la página principal (index.html)
+        window.location.href = 'index.html';
+    } else {
+        // Muestra una alerta de error si faltan campos por llenar
+        showAlertError();
+    }
 }
 
-
-document.getElementById("ingresar").addEventListener("click", iniciar); 
+// Añade un evento de click al botón de "Ingresar" para que llame a la función iniciar
+document.getElementById("ingresar").addEventListener("click", iniciar);
